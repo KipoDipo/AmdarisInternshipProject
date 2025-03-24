7b02681d1730f46623793f2d7b6ce266593571af
kikodimov2002@gmail.com <kikodimov2002@gmail.com>
2025-03-24 18:01:05 +0200

    implemented ICloneable and IEnumerable<T>

--- 3. C# and .NET Basics/Assignment/ComputerComponent.cs
+++ 3. C# and .NET Basics/Assignment/ComputerComponent.cs
@@ -1,21 +1,27 @@
 ﻿namespace Assignment
 {
-	public class ComputerComponent
+	public class ComputerComponent : ICloneable
 	{
         public string Name { get; set; }
         public string Manufacturer { get; set; }
         public decimal Price { get; set; }
-
-        public virtual void DoWork()
-        {
-            Console.WriteLine($"Generic component {Manufacturer} {Name}, doing work!");
-        }
 
         public ComputerComponent(string name, string manufacturer, decimal price)
         {
             Name = name;
             Manufacturer = manufacturer;
             Price = price;
+        }
+
+        public virtual void DoWork()
+        {
+            Console.WriteLine($"Generic component {Manufacturer} {Name}, doing work!");
+        }
+
+        // if a new component class intruduces a mutable type, it should override this method and properly create a deep copy
+        public virtual object Clone()
+        {
+            return MemberwiseClone();
         }
     }
 }
