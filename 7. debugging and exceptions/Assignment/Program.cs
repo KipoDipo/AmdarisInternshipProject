ce9920887b7dd926d950063b6fa25f8ca5353df4
kikodimov2002@gmail.com <kikodimov2002@gmail.com>
2025-03-25 17:47:56 +0200

    uploaded assignment 7

---
+++ 7. debugging and exceptions/Assignment/Program.cs
@@ -0,0 +1,43 @@
+﻿namespace Assignment;
+using static Solution;
+
+public class Program
+{
+    public static void Foo()
+    {
+        var value = 999;
+        try
+        {
+            value = GetAtAndDivideBy(0, 0);
+        }
+        catch (ArgumentOutOfRangeException exception)
+        {
+            Console.WriteLine($"Out of range exception: {exception.Message} ");
+            throw;
+        }
+        catch (DivideByZeroException exception)
+        {
+            Console.WriteLine($"Divide by zero exception: {exception.Message}");
+            throw;
+        }
+        finally
+        {
+            Console.WriteLine($"Value: {value}");
+        }
+    }
+
+    static void Main(string[] args)
+    {
+        try
+        {
+            Foo();
+        }
+        catch (Exception exception)
+        {
+            Console.WriteLine("Go fix the Foo() method...");
+#if(DEBUG)
+            Console.WriteLine($"Full exception:\n{exception}");
+#endif
+        }
+    }
+}
