f7febd43812c9aa7df29c31be180b04f6ae9c69a
kikodimov2002@gmail.com <kikodimov2002@gmail.com>
2025-03-25 16:35:42 +0200

    uploaded assignment 6

---
+++ 6. arrays, collections, generics/Assignment/ComputerComponentsRepository.cs
@@ -0,0 +1,63 @@
+﻿
+namespace Assignment
+{
+    class ComputerComponentsRepository : IRepository<ComputerComponent, uint>
+    {
+        private List<ComputerComponent> components;
+
+        private ComputerComponent Add(ComputerComponent entity)
+        {
+            ComputerComponent toAdd = new ComputerComponent()
+            {
+                Id = entity.Id,
+                Manufacturer = entity.Manufacturer,
+                Name = entity.Name,
+                Price = entity.Price
+            };
+            components.Add(toAdd);
+            return toAdd;
+        }
+
+        public ComputerComponentsRepository()
+        {
+            components = new List<ComputerComponent>();
+        }
+
+        public ComputerComponent Create(ComputerComponent entity)
+        {
+            var existingEntity = components.Find(x => x.Id == entity.Id);
+            if (existingEntity != null)
+                return existingEntity;
+
+            return Add(entity);
+        }
+
+        public void Delete(ComputerComponent entity)
+        {
+            components.RemoveAll(x => x.Id == entity.Id);
+        }
+
+        public IEnumerable<ComputerComponent> Read()
+        {
+            return components;
+        }
+
+        public ComputerComponent? ReadById(uint id)
+        {
+            return components.Find(x => x.Id == id);
+        }
+
+        public ComputerComponent Update(ComputerComponent entity)
+        {
+            var existingEntity = components.Find(x => x.Id == entity.Id);
+            if (existingEntity == null)
+                return Add(entity);
+
+            existingEntity.Manufacturer = entity.Manufacturer;
+            existingEntity.Name = entity.Name;
+            existingEntity.Price = entity.Price;
+
+            return existingEntity;
+        }
+    }
+}
---
+++ 6. arrays, collections, generics/Assignment/Program.cs
@@ -0,0 +1,62 @@
+﻿using Assignment;
+
+ComputerComponentsRepository repository = new ComputerComponentsRepository();
+
+void PrintComponent(ComputerComponent component)
+{
+    Console.WriteLine($"Id: {component.Id}, Manufacturer: {component.Manufacturer}, Name: {component.Name}, Price: {component.Price}");
+}
+
+repository.Create(new ComputerComponent() { Id = 1, Manufacturer = "Intel", Name = "Core i9-11900K", Price = 599.99m });
+repository.Create(new ComputerComponent() { Id = 2, Manufacturer = "AMD", Name = "Ryzen 9 5950X", Price = 799.99m });
+repository.Create(new ComputerComponent() { Id = 3, Manufacturer = "Nvidia", Name = "RTX 3090", Price = 1499.99m });
+repository.Create(new ComputerComponent() { Id = 4, Manufacturer = "Corsair", Name = "Vengeance RGB Pro", Price = 99.99m });
+repository.Create(new ComputerComponent() { Id = 5, Manufacturer = "Samsung", Name = "970 EVO Plus", Price = 149.99m });
+
+foreach (var component in repository.Read())
+{
+    PrintComponent(component);
+}
+
+Console.WriteLine("=======================================");
+
+uint id = 3;
+Console.WriteLine($"Searching for component by ID {id}...");
+var componentById = repository.ReadById(id);
+if (componentById != null)
+{
+    Console.WriteLine("Found!");
+    PrintComponent(componentById);
+}
+else
+{
+    Console.WriteLine("No such component, aborting program...");
+    return;
+}
+
+Console.WriteLine("=======================================");
+
+Console.WriteLine($"Deleting component with ID {id}...");
+repository.Delete(componentById);
+
+foreach (var component in repository.Read())
+{
+    PrintComponent(component);
+}
+
+Console.WriteLine("=======================================");
+
+id = 1;
+Console.WriteLine($"Generating a new identity for component with ID {id}...");
+
+var componentToUpdate = new ComputerComponent() { Id = id, Manufacturer = "Intel", Name = "Core i5-4460", Price = 99.99m };
+
+repository.Update(componentToUpdate);
+
+foreach (var component in repository.Read())
+{
+    PrintComponent(component);
+}
+
+Console.WriteLine("=======================================");
+
