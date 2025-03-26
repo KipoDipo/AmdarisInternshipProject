93342a7f98f7ddf705175742357a8527c431ca9c
kikodimov2002@gmail.com <kikodimov2002@gmail.com>
2025-03-26 12:46:57 +0200

    manipulated via delegates

--- 8. delegates and LINQ/Assignment/Program.cs
+++ 8. delegates and LINQ/Assignment/Program.cs
@@ -1,6 +1,6 @@
 ﻿namespace Assignment
 {
-    class Person
+    public class Person
     {
         public required string FirstName { get; set; }
         public required string LastName { get; set; }
@@ -8,6 +8,19 @@
     
     public class Program
     {
+        delegate void PersonAction(Person person);
+
+        public static void CapitalizeNames(Person person)
+        {
+            person.FirstName = person.FirstName.ToUpper();
+            person.LastName = person.LastName.ToUpper();
+        }
+
+        public static void PrintNames(Person person)
+        {
+            Console.WriteLine($"{person.FirstName} {person.LastName}");
+        }
+
         static void Main(string[] args)
         {
             List<Person> people =
@@ -20,6 +33,19 @@
                 new() { FirstName = "Vasil", LastName = "Karaivanov" },
                 new() { FirstName = "Vladimir", LastName = "Randomirov" },
             ];
+
+            PersonAction action = CapitalizeNames;
+            foreach (var person in people)
+            {
+                action(person);
+            }
+
+            action = PrintNames;
+
+            foreach (var person in people)
+            {
+                action(person);
+            }
         }
     }
 }
