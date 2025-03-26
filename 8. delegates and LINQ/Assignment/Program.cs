deb4fe90841a5cd701fe68276112dd4a3768cf67
kikodimov2002@gmail.com <kikodimov2002@gmail.com>
2025-03-26 12:42:28 +0200

    created collection

---
+++ 8. delegates and LINQ/Assignment/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace Assignment
+{
+    class Person
+    {
+        public required string FirstName { get; set; }
+        public required string LastName { get; set; }
+    }
+    
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Person> people =
+            [
+                new() { FirstName = "Atanas", LastName = "Kambitov" },
+                new() { FirstName = "Georgi", LastName = "Georgiev" },
+                new() { FirstName = "Ivan", LastName = "Ivanov" },
+                new() { FirstName = "Petar", LastName = "Samoilov" },
+                new() { FirstName = "Stefan", LastName = "Stambolov" },
+                new() { FirstName = "Vasil", LastName = "Karaivanov" },
+                new() { FirstName = "Vladimir", LastName = "Randomirov" },
+            ];
+        }
+    }
+}
