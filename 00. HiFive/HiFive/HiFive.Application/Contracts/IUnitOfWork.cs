3d89418991cd091652d208658c9cdf84cb5a4d51
kikodimov2002@gmail.com <kikodimov2002@gmail.com>
2025-04-11 14:58:29 +0300

    Updated Unit of Work interface

--- 00. HiFive/HiFive/HiFive.Application/Contracts/IUnitOfWork.cs
+++ 00. HiFive/HiFive/HiFive.Application/Contracts/IUnitOfWork.cs
@@ -1,14 +1,22 @@
 ﻿using HiFive.Domain.Models.Music;
 using HiFive.Domain.Models.Users;
+using HiFive.Domain.Models.Throphies;
 
 namespace HiFive.Application.Contracts;
 
 public interface IUnitOfWork : IDisposable
 {
+	IRepository<Listener> Listeners { get; }
+	IRepository<Artist> Artists { get; }
+	IRepository<Distributor> Distributors { get; }
+	IRepository<Admin> Admins { get; }
+
 	IRepository<Playlist> Playlists { get; }
 	IRepository<Song> Songs { get; }
 	IRepository<Album> Albums { get; }
-	IRepository<Listener> Listeners { get; }
+
+	IRepository<Badge> Badges { get; }
+	IRepository<Title> Titles { get; }
 
 	Task BeginTransactionAsync();
 	Task CommitTransactionAsync();
