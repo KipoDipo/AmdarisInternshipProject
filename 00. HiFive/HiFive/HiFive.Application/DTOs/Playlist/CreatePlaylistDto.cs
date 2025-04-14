c648eab8d8b18b8bedec2252b5437430600b5162
kikodimov2002@gmail.com <kikodimov2002@gmail.com>
2025-04-14 14:34:26 +0300

    Removed unused DTO

--- 00. HiFive/HiFive/HiFive.Application/DTOs/Playlist/CreatePlaylistDto.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace HiFive.Application.DTOs.Playlist;
-public class CreatePlaylistDto
-{
-	public required string Title { get; set; }
-	public string? Description { get; set; }
-	public required Guid OwnerId { get; set; }
-}
