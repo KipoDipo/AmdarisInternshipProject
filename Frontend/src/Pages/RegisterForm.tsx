0a9c67f55fa1389aea2741a5590708839044e933
kikodimov2002@gmail.com <kikodimov2002@gmail.com>
2025-06-16 13:42:09 +0300

    Registering now allows for optional input for the first and last name, bio and profile picture

--- Frontend/src/Pages/RegisterForm.tsx
+++ Frontend/src/Pages/RegisterForm.tsx
@@ -1,5 +1,5 @@
 import { Button, Stack, Tab, Tabs, Typography } from "@mui/material";
-import { RequiredTextField } from "../Components/TextFields";
+import { OptionalTextField, RequiredTextField } from "../Components/TextFields";
 import { useState } from "react";
 import { RegisterUserRequest } from "../Models/RegisterUser";
 import { useForm } from "react-hook-form";
@@ -11,11 +11,16 @@ type FormFields = {
     displayName: string
     password: string
     repeat: string
+
+    firstName: string
+    lastName: string
+    bio: string,
+    profilePicture: FileList
 }
 
 type AccountType = "Listener" | "Artist" | "Distributor";
 
-function RegisterPage({ onSubmit }: {onSubmit: (type: AccountType, form: RegisterUserRequest) => void }) {
+function RegisterPage({ onSubmit }: { onSubmit: (type: AccountType, form: RegisterUserRequest) => void }) {
     const { register, handleSubmit, watch, formState: { isValid } } = useForm<FormFields>({ mode: 'onChange' })
 
     const [type, setType] = useState(0)
@@ -27,13 +32,18 @@ function RegisterPage({ onSubmit }: {onSubmit: (type: AccountType, form: Registe
     const password = watch('password')
 
     const onClick = (data: FormFields) => {
-        const form = 
-            {
-                email: data.email,
-                username: data.username,
-                displayName: data.displayName,
-                password: data.password,
-            }
+        const form =
+        {
+            email: data.email,
+            username: data.username,
+            displayName: data.displayName,
+            password: data.password,
+            firstName: data.firstName,
+            lastName: data.lastName,
+            bio: data.bio,
+            profilePicture: data.profilePicture[0]
+        }
+
         switch (type) {
             case 0:
                 onSubmit("Listener", form);
@@ -57,29 +67,44 @@ function RegisterPage({ onSubmit }: {onSubmit: (type: AccountType, form: Registe
                     <Tab label='Distributor' />
                 </Tabs>
             </Stack>
-            <RequiredTextField
-                label="Email"
-                {...register('email', { required: true })}
-            />
-            <RequiredTextField
-                label="Username"
-                {...register('username', { required: true })}
-            />
-            <RequiredTextField
-                label="Display Name"
-                {...register('displayName', { required: true })}
-            />
+            <Stack direction='row' gap={4} alignItems='center'>
+                <Stack gap={3}>
+                    <RequiredTextField
+                        label="Email"
+                        {...register('email', { required: true })}
+                    />
+                    <RequiredTextField
+                        label="Username"
+                        {...register('username', { required: true })}
+                    />
+                    <RequiredTextField
+                        label="Display Name"
+                        {...register('displayName', { required: true })}
+                    />
 
-            <RequiredTextField
-                label="Password"
-                type="password"
-                {...register('password', { required: true, minLength: 8 })}
-            />
-            <RequiredTextField
-                label="Repeat password"
-                type="password"
-                {...register('repeat', { required: true, minLength: 8, validate: value => value === password })}
-            />
+                    <RequiredTextField
+                        label="Password"
+                        type="password"
+                        {...register('password', { required: true, minLength: 8 })}
+                    />
+                    <RequiredTextField
+                        label="Repeat password"
+                        type="password"
+                        {...register('repeat', { required: true, minLength: 8, validate: value => value === password })}
+                    />
+                </Stack>
+                <Stack gap={3}>
+                    <OptionalTextField label='First Name' {...register("firstName")} />
+                    <OptionalTextField label='Last Name' {...register("lastName")} />
+                    <OptionalTextField multiline rows={5} label='Bio' {...register("bio")} />
+                    <OptionalTextField
+                        type='file'
+                        label='Profile Picture'
+                        slotProps={{ inputLabel: { shrink: true } }}
+                        {...register('profilePicture')}
+                    />
+                </Stack>
+            </Stack>
             <Button
                 variant="contained"
                 fullWidth
